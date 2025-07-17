da399fdae5b64ede2c89276cbc03f33a19e34105
itsmesaadali <itmesaad@gmail.com>
2025-07-17 14:56:44 +0500

    learn models

---
+++ backend/models/ecommerce/order.models.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose'
+
+const orderItemSchema = new mongoose.Schema(
+    {
+        productId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Product'
+        },
+        quantity: {
+            type: Number,
+            required: true
+        }
+    }
+)
+
+const orderSchema = new mongoose.Schema(
+    {
+        order: {
+            type: Number,
+            required: true
+        },
+        customer: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        ordrItems: {
+            type: [
+                orderItemSchema
+            ]
+        },
+        address:{
+            type:String,
+            required:true
+        },
+        status:{
+            type:String,
+            enum:['PENDING','CANCELLED','DELIVERED'],
+            default:'PENDING',
+        }
+    }, { timestamps: true }
+)
+
+
+export const Order = mongoose.model('Order', orderSchema)
---
+++ backend/models/ecommerce/user.models.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema(
+    {
+        username:{
+            type:String,
+            reuired:true,
+            unique:true,
+            lowercase:true
+        },
+        email:{
+            type:String,
+            reuired:true,
+            unique:true,
+            lowercase:true
+        },
+        password:{
+            type:String,
+            required:true
+        }
+    },{timestamps:true}
+)
+
+export const User = mongoose.model('User',userSchema)
