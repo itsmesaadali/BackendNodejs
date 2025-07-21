f8ab1358cce45a507a5cbc874df20ee5ad636c0a
itsmesaadali <itmesaad@gmail.com>
2025-07-21 13:40:43 +0500

    add user route and learn

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -436,5 +436,6 @@ const getWatchHistory = asyncHandler(async (req, res) => {
 
 
 
+
 export { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory }
 
