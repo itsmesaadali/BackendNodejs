094ed3e9325d987e33b71bc56b0c6360f5bc5dbc
itsmesaadali <itmesaad@gmail.com>
2025-07-23 09:55:18 +0500

    make the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,13 @@
-
-import { useState } from 'react'
-import './App.css'
-import axios from 'axios';
-import { useEffect } from 'react';
-
+import "./App.css";
+import {AppLayout} from './components/Layout/AppLayout'
 function App() {
-  const [jokes, setJokes] = useState([])
-
-  useEffect(() =>{
-    axios.get('/api/jokes')
-    .then((res) =>{
-      setJokes(res.data)
-    })
-    .catch((error) =>{
-      console.log(error)
-    })
-  },[])
-
   return (
     <>
-      <h1>Hello Saad Ali learn backend & frontend</h1>
-      <p>JOKES: {jokes.length} </p>
+      <AppLayout/>
 
-      {
-        jokes.map((joke) =>(
-
-          <div key={joke.id}>
-            <h3>{joke.title}</h3>
-            <p>{joke.content}</p>
-          </div>
-        )
-        )
-      }
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
